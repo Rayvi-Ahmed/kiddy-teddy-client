713ff1d45fb5ec4d1f4e91481707399c68bbe803
Misbah Uddin <rayvi73@gmail.com>
2023-05-19 17:15:12 +0600

    implement search for toy name

--- src/Pages/AllToys/Alltoys.jsx
+++ src/Pages/AllToys/Alltoys.jsx
@@ -3,6 +3,7 @@ import ProductData from '../productData/ProductData';
 
 const Alltoys = () => {
     const [allToys, setAlltoys] = useState([])
+    const [searchText, setSearchText] = useState('')
 
     useEffect(() => {
         fetch('http://localhost:5000/alltoys')
@@ -12,10 +13,18 @@ const Alltoys = () => {
             })
     }, [])
 
+    const handleSearch = () => [
+        fetch(`http://localhost:5000/toySearch/${searchText}`)
+            .then(res => res.json())
+            .then(result => {
+                setAlltoys(result)
+            })
+    ]
+
     return (
         <div>
             <div className='container mx-auto mt-5' >
-                <input type="text" placeholder="Search product name" className="input input-bordered input-secondary w-full max-w-xs " /> <button className='btn bg-pink-800 text-gray-100 mr-4 border-0 hover:bg-pink-400'>Search</button>
+                <input onChange={(e) => setSearchText(e.target.value)} type="text" placeholder="Search product name" className="input input-bordered input-secondary w-full max-w-xs " /> <button onClick={handleSearch} className='btn bg-pink-800 text-gray-100 mr-4 border-0 hover:bg-pink-400'>Search</button>
             </div>
 
             <div className="overflow-x-auto w-full container mx-auto mt-5">
