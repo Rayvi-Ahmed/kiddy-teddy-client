04cadc7bae25823e8ae75b6bd1e7c7816cb011b7
Misbah Uddin <rayvi73@gmail.com>
2023-05-21 09:06:48 +0600

    product details added

--- src/CategoryDetails/CategoryDetails.jsx
+++ src/CategoryDetails/CategoryDetails.jsx
@@ -3,18 +3,20 @@ import { useLoaderData } from 'react-router-dom';
 
 const CategoryDetails = () => {
     const toys = useLoaderData()
-    const { photoUrl, name } = toys
+    const { _id, name, productName, rating, photoUrl, description, Category, email, price, Quantity } = toys
 
 
     return (
-        <section className="dark:bg-gray-800 dark:text-gray-100 container mx-auto">
+        <section className="bg-gray-300 text-gray-700 container mx-auto">
             <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
                 <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
                     <img src={photoUrl} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
                     <div className="p-6 space-y-2 lg:col-span-5">
-                        <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">{name}</h3>
-                        <span className="text-xs dark:text-gray-400">February 19, 2021</span>
-                        <p>Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est in graece fuisset, eos affert putent doctus id.</p>
+                        <h3 className="mb-3 text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">{productName}</h3>
+                        <span className="text-md dark:text-gray-400"><span className='font-bold text-pink-700'>Price :</span>{`$` + price}</span>
+                        <p><span className='font-bold text-pink-700'>Product Description :</span>{description}</p>
+                        <p><span className='font-bold text-pink-700'>Rating : </span>{rating} Star</p>
+                        <p><span className='font-bold text-pink-700'>Saller name : </span> {name}</p>
                     </div>
                 </a>
 
--- src/Pages/ToysDetails/ToyDetails.jsx
+++ src/Pages/ToysDetails/ToyDetails.jsx
@@ -4,11 +4,23 @@ import { useLoaderData, useParams } from 'react-router-dom';
 const ToyDetails = () => {
     const toysInfo = useLoaderData()
     console.log(toysInfo)
-    const { Quantity, name } = toysInfo
+    const { _id, name, productName, rating, photoUrl, description, Category, email, price, Quantity } = toysInfo
     return (
-        <div>
-            <h1>{Quantity}</h1>
-        </div>
+        <section className="bg-gray-300 text-gray-700 container mx-auto">
+            <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+                <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
+                    <img src={photoUrl} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
+                    <div className="p-6 space-y-2 lg:col-span-5">
+                        <h3 className="mb-3 text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">{productName}</h3>
+                        <span className="text-md dark:text-gray-400"><span className='font-bold text-pink-700'>Price :</span>{`$` + price}</span>
+                        <p><span className='font-bold text-pink-700'>Product Description :</span>{description}</p>
+                        <p><span className='font-bold text-pink-700'>Rating : </span>{rating} Star</p>
+                        <p><span className='font-bold text-pink-700'>Saller name : </span> {name}</p>
+                    </div>
+                </a>
+
+            </div>
+        </section>
     );
 };
 
