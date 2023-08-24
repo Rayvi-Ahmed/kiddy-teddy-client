77ff489730ed2c10d5c3c10f9d95f3aa14a7d891
Misbah Uddin <rayvi73@gmail.com>
2023-08-24 15:16:02 +0600

    add transition

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -5,8 +5,8 @@ const Category = ({ category }) => {
 
     return (
         <div>
-            <div className="shadow-lg bg-gray-50 text-gray-800 my-5 hover:scale-110 transition">
-                <img src={photoUrl} alt="" className="object-cover object-center hover:scale-150 transition lg:mx-auto lg:w-[200px] rounded-t-md h-[180px] bg-gray-500" />
+            <div className="shadow-lg bg-gray-50 text-gray-800 my-5 transform hover:scale-110 duration-500">
+                <img src={photoUrl} alt="" className="object-cover object-center transform hover:scale-150 duration-500 lg:mx-auto lg:w-[200px] rounded-t-md h-[180px] bg-gray-500" />
                 <div className="flex flex-col justify-between p-6 space-y-8">
                     <div className="space-y-2 lg:h-[180px]">
                         <h2 className="text-3xl font-semibold tracki">{productName}</h2>
